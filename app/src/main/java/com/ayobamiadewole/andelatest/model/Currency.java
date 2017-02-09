3c0a338c30588070ed78364c29d8aad0dbd2f51e
Ayobami Adewole <ayobami.adewole@gmail.com>
2017-02-09 09:32:01 +0100

    Initial commit

---
+++ app/src/main/java/com/ayobamiadewole/andelatest/model/Currency.java
@@ -0,0 +1,27 @@
+package com.ayobamiadewole.andelatest.model;
+
+/**
+ * Created by aa185298 on 2/9/2017.
+ */
+
+public class Currency {
+
+    private String name;
+    private String currency;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+}
